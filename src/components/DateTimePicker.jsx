086dfdb6719ea0f2591a8d87ad648d9deb49c9e9
Ifajul Islam <135067157+ifajul89@users.noreply.github.com>
2024-10-28 01:42:48 +0600

    added responsive design for date and time picker

--- src/components/DateTimePicker.jsx
+++ src/components/DateTimePicker.jsx
@@ -1,9 +1,10 @@
 import { CgCalendarDates } from "react-icons/cg";
+import { FaAngleDown } from "react-icons/fa6";
 
 const DateTimePicker = () => {
   return (
     <div className="flex justify-between my-5">
-      <div className="join bg-white">
+      <div className="join hidden md:block bg-white">
         <input
           className="join-item btn btn-sm bg-white hover:bg-gray-100 font-light border-0 checked:bg-gray-200"
           type="radio"
@@ -25,9 +26,29 @@ const DateTimePicker = () => {
           aria-label="24 Hours"
         />
       </div>
+      <details className="dropdown md:hidden">
+        <summary className="btn btn-sm bg-white hover:bg-gray-100 font-light border-none">
+          Pick Day
+          <FaAngleDown />
+        </summary>
+        <ul className="menu dropdown-content bg-base-100 mt-1 rounded-xl font-light z-[1] w-max p-2 shadow">
+          <li>
+            <a>12 Months</a>
+          </li>
+          <li>
+            <a>30 Days</a>
+          </li>
+          <li>
+            <a>7 Days</a>
+          </li>
+          <li>
+            <a>24 Hours</a>
+          </li>
+        </ul>
+      </details>
       <button className="btn btn-sm bg-white hover:bg-gray-100 font-light border-none">
-      <CgCalendarDates className="text-lg" />
-      Custom Date
+        <CgCalendarDates className="text-lg" />
+        Custom Date
       </button>
     </div>
   );
